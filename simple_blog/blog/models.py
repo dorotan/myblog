eeded95467f4693afdb15ca7d0374c3bba389b61
Dorota Niezborała <d.niezborala@merixstudio.com>
2018-01-24 21:01:50 +0100

    Change admin.py for blog
    - add  fields to control which and how fields are displayed on the change list page of the admin
    - add querysets and manager with filter
    - add view to blog

--- simple_blog/blog/models.py
+++ simple_blog/blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+	def get_queryset(self):
+		return super(PublishedManager, self).get_queryset().filter(status='published')
+
 # Create your models here.
 class Post(models.Model):
 	STATUS_CHOICES = (
@@ -38,6 +42,8 @@ class Post(models.Model):
 		choices=STATUS_CHOICES,
 		default='draft'
 		)
+	objects = models.Manager()
+	published = PublishedManager()
 
 	class Meta:
 		ordering = ('-publish',)
