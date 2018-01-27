969cf7f3de74666af3f0fc8202e6f2deb576212c
Dorota Niezborała <d.niezborala@merixstudio.com>
2018-01-27 19:01:51 +0100

    Add form

---
+++ simple_blog/blog/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class EmailPostForm(forms.Form):
+	first_name = forms.CharField(max_length=25)
+	surname = forms.CharField(max_lenth=25)
+	email = forms.EmailField()
+	to = forms.EmailField()
+	comments = forms.CharField(required=False, widget=forms.Textarea)
