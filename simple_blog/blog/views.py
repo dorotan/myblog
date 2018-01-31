dc1e4aaddfcb55d6c5b840bd3e20432c0886a52b
Dorota Niezborała <d.niezborala@merixstudio.com>
2018-01-31 20:57:34 +0100

    Add tags
    - add tag name view for related posts
    - change list/html file to include tags

--- simple_blog/blog/views.py
+++ simple_blog/blog/views.py
@@ -5,6 +5,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
+from taggit.models import Tag
 
 # Create your views here.
 
@@ -16,22 +17,29 @@ class PostListView(ListView):
 	
 
 
-# def post_list(request):
-# 	object_list = Post.published.all()
-# 	paginator = Paginator(object_list, 3)
-# 	page = request.GET.get('page')
-# 	try:
-# 		posts = paginator.page(page)
-# 	except PageNotAnInteger:
-# 		posts = paginator.page(1)
-# 	except EmptyPage:
-# 		posts = paginator.page(paginator.num_pages)
-# 	return render(
-# 		request,
-# 		'blog/post/list.html',
-# 		{'page': page,
-# 		'posts': posts}
-# 		)
+def post_list(request, tag_slug=None):
+	object_list = Post.published.all()
+	tag = None
+
+	if tag_slug:
+		tag = get_object_or_404(Tag, slug=tag_slug)
+		object_list = object_list.filter(tags__in=[tag])
+
+	paginator = Paginator(object_list, 3)
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		posts = paginator.page(1)
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
+	return render(
+		request,
+		'blog/post/list.html',
+		{'page': page,
+		'posts': posts,
+		'tag': tag}
+		)
 
 def post_detail(request, year, month, day, post):
 	post = get_object_or_404(
